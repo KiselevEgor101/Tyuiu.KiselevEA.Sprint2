f319ed6ccdfab4b0796c0579b55a25298ccc5c09
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-10-13 18:05:18 +0500

    123

--- Tyuiu.KiselevEA.Sprint2.Task5.V9.Lib/DataService.cs
+++ Tyuiu.KiselevEA.Sprint2.Task5.V9.Lib/DataService.cs
@@ -9,41 +9,41 @@ namespace Tyuiu.KiselevEA.Sprint2.Task5.V9.Lib
             {
                 m++;
             }
-            if ((n < 9) || (m % 2 == 0 && n == 30) || (m % 2 != 0 && n == 31) || (m == 3 && n == 28))
+            if ((m < 9) || (m % 2 == 0 && m == 30) || (m % 2 != 0 && m == 31) || (m == 3 && m == 28))
             {
                 switch (m)
                 {
-                    case 1: return $"0{n + 1}.0{m}";
-                    case 2: return $"0{n % 30 + 1}.0{m}";
-                    case 3: return $"0{n % 28 + 1}.0{m}";
-                    case 4: return $"0{n % 30 + 1}.0{m}";
-                    case 5: return $"0{n % 31 + 1}.0{m}";
-                    case 6: return $"0{n % 30 + 1}.0{m}";
-                    case 7: return $"0{n % 31 + 1}.0{m}";
-                    case 8: return $"0{n % 30 + 1}.0{m}";
-                    case 9: return $"0{n % 31 + 1}.0{m}";
-                    case 10: return $"0{n % 30 + 1}.{m}";
-                    case 11: return $"0{n % 31 + 1}.{m}";
-                    case 12: return $"0{n % 30 + 1}.{m}";
+                    case 1: return $"0{m + 1}.0{n}";
+                    case 2: return $"0{n % 30 + 1}.0{n}";
+                    case 3: return $"0{m % 28 + 1}.0{n}";
+                    case 4: return $"0{m % 30 + 1}.0{n}";
+                    case 5: return $"0{m % 31 + 1}.0{n}";
+                    case 6: return $"0{m % 30 + 1}.0{n}";
+                    case 7: return $"0{m % 31 + 1}.0{n}";
+                    case 8: return $"0{m % 30 + 1}.0{n}";
+                    case 9: return $"0{m % 31 + 1}.0{n}";
+                    case 10: return $"0{m % 30 + 1}.{n}";
+                    case 11: return $"0{m % 31 + 1}.{n}";
+                    case 12: return $"0{m % 30 + 1}.{n}";
                     default: throw new ArgumentException("31 декабря недоступно.");
                 }
             }
             else
             {
-                switch (m)
+                switch (n)
                 {
-                    case 1: return $"{n + 1}.0{m}";
-                    case 2: return $"{n % 30 + 1}.0{m}";
-                    case 3: return $"{n % 28 + 1}.0{m}";
-                    case 4: return $"{n % 30 + 1}.0{m}";
-                    case 5: return $"{n % 31 + 1}.0{m}";
-                    case 6: return $"{n % 30 + 1}.0{m}";
-                    case 7: return $"{n % 31 + 1}.0{m}";
-                    case 8: return $"{n % 30 + 1}.0{m}";
-                    case 9: return $"{n % 31 + 1}.0{m}";
-                    case 10: return $"{n % 30 + 1}.{m}";
-                    case 11: return $"{n % 31 + 1}.{m}";
-                    case 12: return $"{n % 30 + 1}.{m}";
+                    case 1: return $"{m + 1}.0{n}";
+                    case 2: return $"{m % 30 + 1}.0{n}";
+                    case 3: return $"{m % 28 + 1}.0{n}";
+                    case 4: return $"{m % 30 + 1}.0{n}";
+                    case 5: return $"{m % 31 + 1}.0{n}";
+                    case 6: return $"{m % 30 + 1}.0{n}";
+                    case 7: return $"{m % 31 + 1}.0{n}";
+                    case 8: return $"{m % 30 + 1}.0{n}";
+                    case 9: return $"{m % 31 + 1}.0{n}";
+                    case 10: return $"{m % 30 + 1}.{n}";
+                    case 11: return $"{m % 31 + 1}.{n}";
+                    case 12: return $"{m % 30 + 1}.{n}";
                     default: throw new ArgumentException("31 декабря недоступно.");
 
                 }
